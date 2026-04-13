89181081f05b93870c5c41721d85b573d4de9f49
Ismael <id386827@gmail.com>
2026-04-13 23:20:00 +0200

    main socio v.0.0.1

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -1,6 +1,8 @@
 package vista;
 
 import entrada.Teclado;
+import exception.BDException;
+import exception.SociosException;
 import socios.AccesoSocio;
 import socios.Socio;
 
@@ -68,182 +70,198 @@ public class Main {
     public static void main(String[] args) {
         int opcion;
         List<Socio> socios;
+        Socio socio;
 
         do {
             opcion = menuPrincipal();
 
-            switch (opcion) {
+            try {
+                switch (opcion) {
 
-                case 0:
-                    System.out.println("Saliendo del programa...");
-                    break;
+                    case 0:
+                        System.out.println("Saliendo del programa...");
+                        break;
 
-                case 1:
-                    int opLibro;
-                    do {
-                        opLibro = menuLibros();
+                    case 1:
+                        int opLibro;
+                        do {
+                            opLibro = menuLibros();
 
-                        if (opLibro < 0 || opLibro > 6) {
-                            System.out.println("La opción de menú debe estar comprendida entre 0 y 6.");
-                            continue;
-                        }
+                            if (opLibro < 0 || opLibro > 6) {
+                                System.out.println("La opción de menú debe estar comprendida entre 0 y 6.");
+                                continue;
+                            }
 
-                        switch (opLibro) {
+                            switch (opLibro) {
 
-                            case 1:
-                                System.out.println("Insertando un libro en la base de datos...");
-                                String isbn = Teclado.leerCadena("ISBN: ");
-                                String titulo = Teclado.leerCadena("Título: ");
-                                String escritor = Teclado.leerCadena("Escritor: ");
-                                int anio = Teclado.leerEntero("Año: ");
-                                double puntuacion = Teclado.leerNatural("Puntuación: ");
+                                case 1:
+                                    System.out.println("Insertando un libro en la base de datos...");
+                                    String isbn = Teclado.leerCadena("ISBN: ");
+                                    String titulo = Teclado.leerCadena("Título: ");
+                                    String escritor = Teclado.leerCadena("Escritor: ");
+                                    int anio = Teclado.leerEntero("Año: ");
+                                    double puntuacion = Teclado.leerNatural("Puntuación: ");
 
-                                System.out.println("Se ha insertado un libro en la base de datos.");
-                                break;
+                                    System.out.println("Se ha insertado un libro en la base de datos.");
+                                    break;
 
-                            case 2:
-                                System.out.println("Eliminando un libro, por código, de la base de datos...");
-                                int codigo = Teclado.leerEntero("Código libro: ");
+                                case 2:
+                                    System.out.println("Eliminando un libro, por código, de la base de datos...");
+                                    int codigo = Teclado.leerEntero("Código libro: ");
 
-                                break;
+                                    break;
 
-                            case 3:
-                                System.out.println("Consultando todos los libros de la base de datos...");
-                                break;
+                                case 3:
+                                    System.out.println("Consultando todos los libros de la base de datos...");
+                                    break;
 
-                            case 4:
-                                System.out.println("Consultando varios libros, por escritor, de la base de datos, ordenados por puntuación decendente...");
-                                String escritorBusqueda = Teclado.leerCadena("Escritor: ");
+                                case 4:
+                                    System.out.println("Consultando varios libros, por escritor, de la base de datos, ordenados por puntuación decendente...");
+                                    String escritorBusqueda = Teclado.leerCadena("Escritor: ");
 
-                                break;
+                                    break;
 
-                            case 5:
-                                System.out.println("Consultando los libros no prestados de la base de datos...");
+                                case 5:
+                                    System.out.println("Consultando los libros no prestados de la base de datos...");
 
-                                break;
+                                    break;
 
-                            case 6:
-                                System.out.println("Consultando los libros devueltos, en una fecha, de la base de datos...");
+                                case 6:
+                                    System.out.println("Consultando los libros devueltos, en una fecha, de la base de datos...");
 
-                                String fecha = Teclado.leerCadena("Fecha devolución: ");
+                                    String fecha = Teclado.leerCadena("Fecha devolución: ");
 
-                                break;
-                        }
+                                    break;
+                            }
 
-                    } while (opLibro != 0);
-                    break;
+                        } while (opLibro != 0);
+                        break;
 
-                case 2:
-                    int opSocio;
-                    do {
-                        opSocio = menuSocios();
+                    case 2:
+                        int opSocio;
+                        do {
+                            opSocio = menuSocios();
 
-                        if (opSocio < 0 || opSocio > 6) {
-                            System.out.println("La opción de menú debe estar comprendida entre 0 y 6.");
-                            continue;
-                        }
+                            if (opSocio < 0 || opSocio > 6) {
+                                System.out.println("La opción de menú debe estar comprendida entre 0 y 6.");
+                                continue;
+                            }
 
-                        switch (opSocio) {
+                            switch (opSocio) {
+                                case 1:
+                                    System.out.println("Insertando un socio en la base de datos...");
 
-                            case 1:
-                                System.out.print("Insertando un socio en la base de datos...");
+                                    String dni = Teclado.leerCadena("DNI: ");
+                                    String nombre = Teclado.leerCadena("Nombre: ");
+                                    String domicilio = Teclado.leerCadena("Domicilio: ");
+                                    String telefono = Teclado.leerCadena("Teléfono: ");
+                                    String correo = Teclado.leerCadena("Correo: ");
 
-                                String dni = Teclado.leerCadena("DNI: ");
-                                String nombre = Teclado.leerCadena("Nombre: ");
-                                String domicilio = Teclado.leerCadena("Domicilio: ");
-                                String telefono = Teclado.leerCadena("Teléfono: ");
-                                String correo = Teclado.leerCadena("Correo: ");
+                                    socio = new Socio(0, dni, nombre, domicilio, telefono, correo);
+                                    boolean insertado = AccesoSocio.insertarSocio(socio);
 
-                                break;
+                                    if (insertado) {
+                                        System.out.println("Se ha insertado un socio en la base de datos.");
+                                    } else {
+                                        System.out.println("No se ha insertado el socio en la base de datos.");
+                                    }
+                                    break;
+                                case 2:
+                                    System.out.println("Eliminando un socio, por código, de la base de datos....");
+                                    int codigoSocio = Teclado.leerEntero("Código socio: ");
 
-                            case 2:
-                                System.out.print("Eliminando un socio, por código, de la base de datos....");
+                                    AccesoSocio.eliminarSocio(codigoSocio);
 
-                                int codigoSocio = Teclado.leerEntero("Código socio: ");
+                                    System.out.println("Se ha eliminado un socio de la base de datos.");
+                                    break;
+                                case 3:
+                                    System.out.println("Consultando todos los socios de la base de datos...");
+                                    socios = AccesoSocio.consultarSocios();
 
-                                break;
+                                    System.out.println(AccesoSocio.toStringList(socios));
+                                    System.out.println("Se han encontrado " + socios.size() + " socios en la  base de datos.");
+                                    break;
 
-                            case 3:
-                                System.out.println("Consultando todos los socios de la base de datos...");
-                                socios = AccesoSocio.consultarSocios();
+                                case 4:
+                                    System.out.println("Consultando varios socios, por localidad, de la base de datos, ordenados por nombre ascendente");
+                                    String localidad = Teclado.leerCadena("Localidad: ");
 
-                                System.out.println(AccesoSocio.toStringList(socios));
-                                System.out.println("Se han encontrado " + socios.size() + " socios en la  base de datos.");
-                                break;
+                                    socios = AccesoSocio.consultarSociosPorLocalidadOrdenadoPorNombreAsc(localidad);
+                                    System.out.println(AccesoSocio.toStringList(socios));
+                                    System.out.println("Se han encontrado " + socios.size() + " socios en la  base de datos.");
+                                    break;
 
-                            case 4:
-                                System.out.println("Consultando varios socios, por localidad, de la base de datos, ordenados por nombre ascendente");
+                                case 5:
+                                    System.out.println("Consultando los socios sin préstamos de la base de datos...");
+                                    socios = AccesoSocio.consultarSociosSinPrestamos();
 
-                                String localidad = Teclado.leerCadena("Localidad: ");
+                                    System.out.println(AccesoSocio.toStringList(socios));
+                                    System.out.println("Se han encontrado " + socios.size() + " socios en la  base de datos.");
+                                    break;
+                                case 6:
+                                    System.out.println("Consultando los socios con préstamos en una fecha de la base de datos...");
+                                    String fechaPrestamo = Teclado.leerCadena("Fecha: ");
+                                    socios = AccesoSocio.consultarSociosPorPrestamoEnFecha(fechaPrestamo);
 
-                                break;
+                                    System.out.println(AccesoSocio.toStringList(socios));
+                                    System.out.println("Se han encontrado " + socios.size() + " socios en la base de datos.");
+                                    break;
+                            }
 
-                            case 5:
-                                System.out.println("Consultando los socios sin préstamos de la base de datos...");
-                                break;
+                        } while (opSocio != 0);
+                        break;
 
-                            case 6:
-                                System.out.println("Consultando los socios con préstamos en una fecha de la base de datos...");
-                                String fechaPrestamo = Teclado.leerCadena("Fecha: ");
-                                socios = AccesoSocio.consultarSociosPorPrestamoEnFecha(fechaPrestamo);
+                    case 3:
+                        int opPrestamo;
+                        do {
+                            opPrestamo = menuSocios();
 
-                                System.out.println(AccesoSocio.toStringList(socios));
-                                System.out.println("Se han encontrado " +  socios.size() + " socios en la base de datos.");
-                                break;
-                        }
+                            if (opPrestamo < 0 || opPrestamo > 6) {
+                                System.out.println("La opción de menú debe estar comprendida entre 0 y 6.");
+                                continue;
+                            }
 
-                    } while (opSocio != 0);
-                    break;
+                            switch (opPrestamo) {
 
-                case 3:
-                    int opPrestamo;
-                    do {
-                        opPrestamo = menuSocios();
+                                case 1:
+                                    System.out.print("Insertando un préstamo en la base de datos...");
 
-                        if (opPrestamo < 0 || opPrestamo > 6) {
-                            System.out.println("La opción de menú debe estar comprendida entre 0 y 6.");
-                            continue;
-                        }
+                                    break;
 
-                        switch (opPrestamo) {
+                                case 2:
+                                    System.out.print("Actualizando un préstamo, por datos identificativos, de la base de datos...");
 
-                            case 1:
-                                System.out.print("Insertando un préstamo en la base de datos...");
+                                    break;
 
-                                break;
+                                case 3:
+                                    System.out.println("Eliminando un préstamo, por datos identificativos, de la base de datos...");
 
-                            case 2:
-                                System.out.print("Actualizando un préstamo, por datos identificativos, de la base de datos...");
+                                    break;
 
-                                break;
+                                case 4:
+                                    System.out.println("Consultando todos los préstamos de la base de datos...");
 
-                            case 3:
-                                System.out.println("Eliminando un préstamo, por datos identificativos, de la base de datos...");
+                                    break;
 
-                                break;
+                                case 5:
+                                    System.out.println("Consultando los préstamos no devueltos de la base de datos....");
+                                    break;
 
-                            case 4:
-                                System.out.println("Consultando todos los préstamos de la base de datos...");
+                                case 6:
+                                    System.out.print("Consultando DNI y nombre de socio, ISBN y título de libro y fecha de devolución de los préstamos realizados en una fecha de la base de datos...");
 
-                                break;
+                                    break;
+                            }
 
-                            case 5:
-                                System.out.println("Consultando los préstamos no devueltos de la base de datos....");
-                                break;
+                        } while (opPrestamo != 0);
+                        break;
 
-                            case 6:
-                                System.out.print("Consultando DNI y nombre de socio, ISBN y título de libro y fecha de devolución de los préstamos realizados en una fecha de la base de datos...");
-
-                                break;
-                        }
-
-                    } while (opPrestamo != 0);
-                    break;
-
-                default:
-                    System.out.println("La opción de menú debe estar comprendida entre 0 y 3.");
+                    default:
+                        System.out.println("La opción de menú debe estar comprendida entre 0 y 3.");
+                }
+            } catch (BDException | SociosException e) {
+                System.err.println(e.getMessage());
             }
-
         } while (opcion != 0);
     }
 }
