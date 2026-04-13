e4ed1f573bf82afff74d404eb40e8b83392943df
Ismael <id386827@gmail.com>
2026-04-13 22:14:12 +0200

    SociosException v.0.0.1

--- src/exception/SociosException.java
+++ src/exception/SociosException.java
@@ -3,6 +3,9 @@ package exception;
 public class SociosException extends RuntimeException {
     public static final String SOCIO_INEXISTENTE = "No existe ningún socio con ese código en la base de datos.";
     public static final String SOCIOS_NO_ENCONTRADOS = "No se ha encontrado ningún socio en la base de datos.";
+    public static final String SOCIOS_NO_ENCONTRADOS_LOCALIDAD = "No existe ningún socio con esa localidad en la base de datos.";
+    public static final String SOCIOS_NO_ENCONTRADOS_SIN_PRESTAMOS = "No existe ningún socio sin préstamos en la base de datos.";
+    public static final String SOCIOS_NO_ENCONTRADOS_CON_PRESTAMOS = "No existe ningún socio con préstamos en esa fecha en la base de datos.";
 
     public SociosException(String message) {
         super(message);
