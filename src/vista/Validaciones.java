bceaa15d4a4039bda5c1aaed8a2fdcff0149a85a
Ismael <id386827@gmail.com>
2026-04-21 18:21:33 +0200

    Validaciones v.0.0.2

--- src/vista/Validaciones.java
+++ src/vista/Validaciones.java
@@ -12,4 +12,16 @@ public class Validaciones {
     public static boolean validarCorreo(String correo){
         return correo.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
     }
+
+    public static boolean validarTelefono(String telefono){
+        return telefono.matches("^6[0-9]{8}$");
+    }
+
+    public static boolean validarNombre(String nombre){
+        return nombre.matches("^[a-zA-Z]\\w+$");
+    }
+
+    public static boolean validarFecha(String fecha){
+        return fecha.matches("^\\d{2}-\\d{2}-\\d{2}$");
+    }
 }
